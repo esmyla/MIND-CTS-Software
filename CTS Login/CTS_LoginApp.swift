70ae03e3393650ee27935752bbfa7786d04ec9b8
Divya Manvikar <divya.manvikar@gmail.com>
2026-02-19 19:32:55 -0500

    Initial Commit

---
+++ CTS Login/CTS_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTS_LoginApp.swift
+//  CTS Login
+//
+//  Created by Divya Manvikar on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CTS_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
