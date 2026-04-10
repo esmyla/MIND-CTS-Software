45073d66560e3148562f8f8324fd2e0c8edf4bbd
Paul-Purdue <pharkenr@purdue.edu>
2026-04-09 20:52:35 -0400

    adding flexion

---
+++ app/PT_accuracy/hand_flexion_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\harkp\\.vscode\\extensions\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\harkp\\.vscode\\extensions\\flutter"
+  "PROJECT_DIR=C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app"
+  "FLUTTER_ROOT=C:\\Users\\harkp\\.vscode\\extensions\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app"
+  "FLUTTER_TARGET=C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app\\lib\\main.dart"
+  "DART_DEFINES=U1VQQUJBU0VfVVJMPWh0dHBzOi8vY21tdW13d3p5ZGZlYmFoaGdmeWkuc3VwYWJhc2UuY28=,U1VQQUJBU0VfQU5PTl9LRVk9ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKemRYQmhZbUZ6WlNJc0luSmxaaUk2SW1OdGJYVnRkM2Q2ZVdSbVpXSmhhR2huWm5scElpd2ljbTlzWlNJNkltRnViMjRpTENKcFlYUWlPakUzTmpJNE1Ea3dOVEFzSW1WNGNDSTZNakEzT0RNNE5UQTFNSDAuekpCaTBvd0tvYXljTnptdEFtOV81WnNVd1hJVW14QUd1Q3kwQWhzYW9aYw==,RkVBVFVSRV9BVVRIPXRydWU=,RkxVVFRFUl9WRVJTSU9OPTMuMzguOQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjczMjNkZTI4NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTg3YzE4Zjg3Mw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC44"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\harkp\\MIND-CTS-Software\\app\\PT_accuracy\\hand_flexion_app\\.dart_tool\\package_config.json"
+)
